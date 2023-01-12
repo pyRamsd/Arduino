77c32f134c6f3f20faf0db0baeaa86bbe44b8163
pyRamsd <minauzum56@gmail.com>
2023-01-12 10:13:10 -0500

    proyecto #1

---
+++ Encender_led/Encender_led.ino
@@ -0,0 +1,10 @@
+# define GREEN 9
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(GREEN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(GREEN, HIGH);
+}
